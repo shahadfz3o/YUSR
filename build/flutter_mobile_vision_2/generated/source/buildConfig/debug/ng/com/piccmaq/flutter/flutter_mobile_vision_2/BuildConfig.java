9262a71386120cc109f58aa01c6e164cd9bbe273
shahad <shahad.fz3o@gmail.com>
2023-06-19 18:40:40 +0300

    Add files via upload

---
+++ build/flutter_mobile_vision_2/generated/source/buildConfig/debug/ng/com/piccmaq/flutter/flutter_mobile_vision_2/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ng.com.piccmaq.flutter.flutter_mobile_vision_2;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "ng.com.piccmaq.flutter.flutter_mobile_vision_2";
+  public static final String BUILD_TYPE = "debug";
+}
